75022a1da15c433cb427489597f5c0670a1d71bd
Ross Newman <ross.newman@defencex.ai>
2025-01-13 11:07:09 +1000

    Adding examples

---
+++ examples/mediax_rtp_sap_transmit/rtp_sap_transmit.cc
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2024, Astute Systems PTY LTD
+//
+// This file is part of the VivoeX project developed by Astute Systems.
+//
+// Licensed under the Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
+// License. See the LICENSE file in the project root for full license details.
+//
+/// \brief A SAP/SDP listener example
+///
+/// \file rtp_sap_transmit.cc
+///
+
+#include "rtp/rtp.h"
+
+int main(int argc, char *argv[])
+{
+  mediax::RtpSapTransmit<mediax::rtp::uncompressed::RtpUncompressedPayloader> rtp("238.192.1.1", 5004, "test-session-name", 640,
+                                                                                  480, 25, "AV1");
+  std::vector<uint8_t> &data = rtp.GetBufferTestPattern(10); // Bouncing ball
+  while (true)
+    rtp.Transmit(data.data(), false);
+}
